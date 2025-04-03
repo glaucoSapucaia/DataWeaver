7cfcc481bfb7301b6eaf1ab4c0a52c5ef5ab127a
glaucoSapucaia <glaucopco@gmail.com>
2025-04-03 08:03:28 -0300

    update: web_scraper utils/get_env.py - check None value

--- app/src/utils/get_env.py
+++ app/src/utils/get_env.py
@@ -5,8 +5,12 @@ def get_env_variable(var_name: str, default: str) -> str:
     Obtém uma variável de ambiente, garantindo que ela tenha um valor válido.
     
     :param var_name: Nome da variável de ambiente
-    :param default: Valor padrão caso a variável não esteja definida
+    :param default: Valor padrão caso a variável não esteja definida ou seja inválida
     :return: Valor da variável de ambiente ou o padrão
     """
-    value = os.getenv(var_name, default).strip()
-    return value if value else default
+    value = os.getenv(var_name)
+    
+    if value is None or not value.strip():  # Garante que a variável não está vazia
+        return default
+
+    return value.strip()
